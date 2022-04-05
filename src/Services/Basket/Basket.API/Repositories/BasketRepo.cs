8adfcee69a89ea33f5f874295cc1d09bfcbf14fe
Daniels-CIDE <73656233+Daniels-CIDE@users.noreply.github.com>
2022-04-05 11:16:30 +0200

    basket.api(not finished)

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepo.cs
@@ -0,0 +1,40 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepo : IBasketRepo
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepo(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+
+        public async Task<ShoppingCart> GetBasket(string username)
+        {
+            var basket = await _redisCache.GetStringAsync(username);
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string username)
+        {
+            await _redisCache.RemoveAsync(username);
+        }
+    }
+}
